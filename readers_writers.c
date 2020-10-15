5991adb51ab0d8ad4f3f7cb2dc8ac688d38fb428
Fedor Uvarov <acharvak@fsfe.org>
2020-10-16 00:50:38 +0300

    Add heap allocation

--- readers_writers.c
+++ readers_writers.c
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <processthreadsapi.h>
 #include <synchapi.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,25 +11,32 @@
 #define NUMBER_OF_WRITERS 3
 #define MAXIMUM_VALUE 10
 
-// Я кастую между int и LPVOID очень аккуратно. Достаточно аккуратно для тестового задания, по крайней мере.
-#pragma warning(disable : 4311)
-#pragma warning(disable : 4312)
-
 // Нормальные люди, конечно, берут готовый SRWLock из synchapi.h,
 // но по заданию надо это делать вручную, причём с применением mutex
 // и event. Поэтому вот вам вручную и на чистом C (должно компилироваться и как C++).
-HANDLE hEntryEvent;
-HANDLE hWaitingReadersEvent;
-HANDLE hWaitingWritersEvent;
 
-int ActiveReaders = 0;
-int WaitingReaders = 0;
-int WaitingWriters = 0;
+// Согласно присланным требованиям, надо выделять память в куче
+typedef struct SharedData {
+    int TheValue;
+    DWORD LastWriter;
+    
+    HANDLE hEntryEvent;
+    HANDLE hWaitingReadersEvent;
+    HANDLE hWaitingWritersEvent;
 
-int LastWriter = 0;
-int TheValue = 0;
+    int ActiveReaders;
+    int WaitingReaders;
+    int WaitingWriters;
 
-unsigned long LastEvent = 0;    // Для протоколирования
+    unsigned long LastEvent;    // Для протоколирования
+} SharedData;
+
+
+typedef struct WriterLaunchInfo {
+    int number;
+    SharedData* ptrsdata;
+} WriterLaunchInfo;
+
 
 HANDLE hPrintMutex;     // Для непрерывных строк вывода
 
@@ -81,7 +89,8 @@ HANDLE _CreateThread(LPTHREAD_START_ROUTINE lpStartAddress, LPVOID lpParameter) 
 
 
 DWORD WINAPI ReaderProc(_In_ LPVOID lpParameter) {
-    int my_number = (int) lpParameter;
+    SharedData* ptrsdata = (SharedData*) lpParameter;
+    DWORD my_id = GetCurrentThreadId();
     BOOL must_continue = TRUE;
 
     while(must_continue) {
@@ -89,42 +98,42 @@ DWORD WINAPI ReaderProc(_In_ LPVOID lpParameter) {
     
         unsigned long my_event;
         // Берём читательский захват
-        _WaitForSingleObject(hEntryEvent);
-        if(WaitingWriters) {
+        _WaitForSingleObject(ptrsdata->hEntryEvent);
+        if(ptrsdata->WaitingWriters) {
             // Надо ждать писателей
-            ++WaitingReaders;
-            _SignalObjectAndWait(hEntryEvent, hWaitingReadersEvent);
-            --WaitingReaders;
+            ++ptrsdata->WaitingReaders;
+            _SignalObjectAndWait(ptrsdata->hEntryEvent, ptrsdata->hWaitingReadersEvent);
+            --ptrsdata->WaitingReaders;
         }
-        ++ActiveReaders;
-        my_event = ++LastEvent;
+        ++ptrsdata->ActiveReaders;
+        my_event = ++ptrsdata->LastEvent;
         
         // Выпускаем следующего читателя (или писателя на вход)
-        if(WaitingReaders) {
-            _SetEvent(hWaitingReadersEvent);
+        if(ptrsdata->WaitingReaders) {
+            _SetEvent(ptrsdata->hWaitingReadersEvent);
         } else {
-            _SetEvent(hEntryEvent);
+            _SetEvent(ptrsdata->hEntryEvent);
         }
         
         // Считываем значение
-        int my_last_writer = LastWriter;
-        int my_value = TheValue;
+        DWORD my_last_writer = ptrsdata->LastWriter;
+        int my_value = ptrsdata->TheValue;
 
         // Отпускаем читательский захват
-        _WaitForSingleObject(hEntryEvent);
-        --ActiveReaders;
-        if(!ActiveReaders && WaitingWriters) {
-            _SetEvent(hWaitingWritersEvent);
+        _WaitForSingleObject(ptrsdata->hEntryEvent);
+        --ptrsdata->ActiveReaders;
+        if(!ptrsdata->ActiveReaders && ptrsdata->WaitingWriters) {
+            _SetEvent(ptrsdata->hWaitingWritersEvent);
         } else {
-            _SetEvent(hEntryEvent);
+            _SetEvent(ptrsdata->hEntryEvent);
         }
 
         // Выводим результат
         _WaitForSingleObject(hPrintMutex);
         if(my_value < MAXIMUM_VALUE) {
-            printf("%08lu: Reader %d read %d left by writer %d\r\n", my_event, my_number, my_value, my_last_writer);
+            printf("%08lu: Reader %d read %d left by writer %u\r\n", my_event, my_id, my_value, my_last_writer);
         } else {
-            printf("%08lu: Reader %d read %d left by writer %d and exited\r\n", my_event, my_number, my_value, my_last_writer);
+            printf("%08lu: Reader %d read %d left by writer %u and exited\r\n", my_event, my_id, my_value, my_last_writer);
             must_continue = FALSE;
         }
         _ReleaseMutex(hPrintMutex);
@@ -135,9 +144,13 @@ DWORD WINAPI ReaderProc(_In_ LPVOID lpParameter) {
 
 
 DWORD WINAPI WriterProc(_In_ LPVOID lpParameter) {
-    int my_number = (int) lpParameter;
+    WriterLaunchInfo* ptrwli = (WriterLaunchInfo*) lpParameter;
+    SharedData* ptrsdata = ptrwli->ptrsdata;
+    DWORD my_id = GetCurrentThreadId();
     BOOL must_continue = TRUE;
-    DWORD wait_ms = my_number * WRITER_WAIT_MS;
+    DWORD wait_ms = ptrwli->number * WRITER_WAIT_MS; // Раньше тут были некоторые оптимизации, но после переделок получилось так
+    free(ptrwli);
+    ptrwli = NULL;
 	
     while(must_continue) {
         Sleep(wait_ms);
@@ -146,38 +159,38 @@ DWORD WINAPI WriterProc(_In_ LPVOID lpParameter) {
         unsigned long my_event;
         int written_value = 0;
         // Берём полный захват
-        _WaitForSingleObject(hEntryEvent);
-        if(ActiveReaders) {
+        _WaitForSingleObject(ptrsdata->hEntryEvent);
+        if(ptrsdata->ActiveReaders) {
             // Ждём читателей
-            ++WaitingWriters;
-            _SignalObjectAndWait(hEntryEvent, hWaitingWritersEvent);
-            --WaitingWriters;
+            ++ptrsdata->WaitingWriters;
+            _SignalObjectAndWait(ptrsdata->hEntryEvent, ptrsdata->hWaitingWritersEvent);
+            --ptrsdata->WaitingWriters;
         }
-        my_event = ++LastEvent;
+        my_event = ++ptrsdata->LastEvent;
         
         // Записываем
-        if(TheValue < MAXIMUM_VALUE) {
-            written_value = ++TheValue;
-            LastWriter = my_number;
+        if(ptrsdata->TheValue < MAXIMUM_VALUE) {
+            written_value = ++ptrsdata->TheValue;
+            ptrsdata->LastWriter = my_id;
         } else {
             must_continue = FALSE;
         }
         
         // Выпускаем следующего
-        if(WaitingWriters) {
-            _SetEvent(hWaitingWritersEvent);
-        } else if(WaitingReaders) {
-            _SetEvent(hWaitingReadersEvent);
+        if(ptrsdata->WaitingWriters) {
+            _SetEvent(ptrsdata->hWaitingWritersEvent);
+        } else if(ptrsdata->WaitingReaders) {
+            _SetEvent(ptrsdata->hWaitingReadersEvent);
         } else {
-            _SetEvent(hEntryEvent);
+            _SetEvent(ptrsdata->hEntryEvent);
         }
         
         // Выводим результат
         _WaitForSingleObject(hPrintMutex);
         if(must_continue) {
-            printf("%08lu: Writer %d wrote %d\r\n", my_event, my_number, written_value);
+            printf("%08lu: Writer %u wrote %d\r\n", my_event, my_id, written_value);
         } else {
-            printf("%08lu: Writer %d exits because the maximum value has been reached\r\n", my_event, my_number);
+            printf("%08lu: Writer %u exits because the maximum value has been reached\r\n", my_event, my_id);
         }
         _ReleaseMutex(hPrintMutex);
     }
@@ -189,9 +202,15 @@ DWORD WINAPI WriterProc(_In_ LPVOID lpParameter) {
 #define num_threads NUMBER_OF_WRITERS + NUMBER_OF_READERS
 int main(int argc, char** argv) {
     HANDLE threads[num_threads];
-    hEntryEvent = _CreateEvent(TRUE);
-    hWaitingReadersEvent = _CreateEvent(FALSE);
-    hWaitingWritersEvent = _CreateEvent(FALSE);
+    
+    SharedData* ptrsdata = calloc(1, sizeof(SharedData));
+    if(!ptrsdata) {
+        handleFailure("calloc");
+    }
+    ptrsdata->hEntryEvent = _CreateEvent(TRUE);
+    ptrsdata->hWaitingReadersEvent = _CreateEvent(FALSE);
+    ptrsdata->hWaitingWritersEvent = _CreateEvent(FALSE);
+    
     hPrintMutex = CreateMutex(NULL, FALSE, NULL);
     if(!hPrintMutex) {
         handleFailure("CreateMutex");
@@ -199,13 +218,17 @@ int main(int argc, char** argv) {
     printf("The maximum value to write is %d\r\n", MAXIMUM_VALUE);
     printf("Launching %d writers\r\n", NUMBER_OF_WRITERS);
     for(int i = 1; i <= NUMBER_OF_WRITERS; ++i) {
-        threads[i - 1] = _CreateThread(&WriterProc, (LPVOID) i);
+        WriterLaunchInfo* ptrwli = malloc(sizeof(WriterLaunchInfo));
+        ptrwli->number = i;
+        ptrwli->ptrsdata = ptrsdata;
+        threads[i - 1] = _CreateThread(&WriterProc, ptrwli);
+        // ptrwli освобождается писателем
     }
     _WaitForSingleObject(hPrintMutex);
     printf("Launching %d readers\r\n", NUMBER_OF_READERS);
     _ReleaseMutex(hPrintMutex);
     for(int i = 1; i <= NUMBER_OF_READERS; ++i) {
-        threads[NUMBER_OF_WRITERS + i - 1] = _CreateThread(&ReaderProc, (LPVOID) i);
+        threads[NUMBER_OF_WRITERS + i - 1] = _CreateThread(&ReaderProc, ptrsdata);
     }
     
     // Ждём
@@ -217,12 +240,13 @@ int main(int argc, char** argv) {
     }
     
     // Для аккуратности
-    HANDLE handles[] = {hEntryEvent, hWaitingReadersEvent, hWaitingWritersEvent, hPrintMutex};
+    HANDLE handles[] = {ptrsdata->hEntryEvent, ptrsdata->hWaitingReadersEvent, ptrsdata->hWaitingWritersEvent, hPrintMutex};
     for(size_t i = 0; i < sizeof(handles) / sizeof(HANDLE); ++i) {
         CloseHandle(handles[i]);
     }
     for(int i = 0; i < num_threads; ++i) {
         CloseHandle(threads[i]);
     }
+    free(ptrsdata);
 	return 0;
 }
